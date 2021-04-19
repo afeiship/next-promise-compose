188b8487db8fd570285bd62db8133ee5d789c917
afeiship <1290657123@qq.com>
2021-04-19 21:11:35 +0800

    feat: initial

---
+++ __tests__/index.spec.js
@@ -0,0 +1,70 @@
+(function () {
+  require('../src');
+
+  // jest.setTimeout(60 * 10000);
+
+  const fetch = require('node-fetch');
+
+  describe('api.basic test', () => {
+    test('pure sync functions', (done) => {
+      const double = (x) => x * 2;
+      const square = (x) => x * x;
+      const add = (x) => x + 100;
+      const fn = nx.promiseCompose(double, square, add);
+      fn(10).then((res) => {
+        expect(res).toBe(500);
+        done()
+      });
+    });
+
+    test('pure async functions', (done) => {
+      const fn1 = (data) => {
+        return Promise.resolve(data + 1);
+      };
+
+      const fn2 = (data) => {
+        return Promise.resolve(data + 'true');
+      };
+
+      const fn3 = (data) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(data + ' ms');
+          }, 100);
+        });
+      };
+
+      const fn = nx.promiseCompose(fn1, fn2, fn3);
+
+      fn(100).then((res) => {
+        expect(res).toBe('101true ms');
+        done();
+      });
+    });
+  });
+
+  test('sync/async mixin functions', (done) => {
+    var fn1 = (data) => {
+      return data + 1;
+    };
+
+    var fn2 = (data) => {
+      return data + 'string';
+    };
+
+    var fn3 = (data) => {
+      return fetch('https://api.github.com/users/afeiship')
+        .then((res) => res.json())
+        .then((res) => {
+          return Promise.resolve(data + res.login);
+        });
+    };
+
+    var fn = nx.promiseCompose(fn1, fn2, fn3);
+    fn(100).then((res) => {
+      // console.log('promiseCompose result:', res);
+      expect(res).toBe('101stringafeiship');
+      done();
+    });
+  });
+})();
---
+++ src/index.js
@@ -0,0 +1,19 @@
+(function () {
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
+  var nx = global.nx || require('@jswork/next');
+  //@thanks to: https://github.com/gr0uch/promise-compose/blob/master/index.js
+  //@thanks to: https://itnext.io/roll-your-own-async-compose-pipe-functions-658cafe4c46f
+
+  nx.promiseCompose = function () {
+    var fns = nx.slice(arguments);
+    return function (inInitialValue) {
+      return fns.reduce(function (chain, fn) {
+        return chain.then(fn);
+      }, Promise.resolve(inInitialValue));
+    };
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.promiseCompose;
+  }
+})();
